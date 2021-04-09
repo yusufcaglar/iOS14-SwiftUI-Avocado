83ee532da824e20d1e874512a9c7b757237fdad3
Yusuf Çağlar <faf7faf7@gmail.com>
2021-04-09 22:08:52 +0300

    Initial Commit

---
+++ Avocado/AvocadoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoApp.swift
+//  Avocado
+//
+//  Created by Yusuf Çağlar on 9.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Avocado/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Avocado
+//
+//  Created by Yusuf Çağlar on 9.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
