c8128924cedeae092220285c6ad046061ae9e6e8
Yusuf Çağlar <faf7faf7@gmail.com>
2021-04-11 22:32:57 +0300

    Add DishesView.

--- Avocado/View/ContentView.swift
+++ Avocado/View/ContentView.swift
@@ -25,18 +25,25 @@ struct ContentView: View {
                     }
                 }
                 
+                //MARK: - Dishes
+                Text("Avocado Dishes")
+                    .fontWeight(.bold)
+                    .modifier(TitleModifier())
+                
+                DishesView()
+                    .frame(maxWidth: 640)
+                
                 //MARK: - Footer
                 VStack(alignment: .center, spacing: 20) {
                     Text("All About Avocado")
-                        .font(.system(.title, design: .serif))
                         .fontWeight(.bold)
-                        .foregroundColor(Color("ColorGreenAdaptive"))
-                        .padding(8)
+                        .modifier(TitleModifier())
                     
                     Text("Everything you wanted to know about avocados but were too afraid to ask.")
                         .font(.system(.body, design: .serif))
                         .multilineTextAlignment(.center)
                         .foregroundColor(.gray)
+                        .frame(minHeight: 60)
                 }
                 .frame(maxWidth: 640)
                 .padding()
@@ -45,6 +52,15 @@ struct ContentView: View {
         }
         .edgesIgnoringSafeArea(.all)
         .padding(0)
+    }
+}
+
+struct TitleModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.system(.title, design: .serif))
+                .foregroundColor(Color("ColorGreenAdaptive"))
+                .padding(8)
     }
 }
 //MARK: - Preview
---
+++ Avocado/View/DishesView.swift
@@ -0,0 +1,137 @@
+//
+//  DishesView.swift
+//  Avocado
+//
+//  Created by Yusuf Çağlar on 11.04.2021.
+//
+
+import SwiftUI
+
+struct DishesView: View {
+    //MARK: - Properties
+    
+    //MARK: - Body
+    
+    var body: some View {
+        
+        HStack(alignment: .center, spacing: 4) {
+            
+            //MARK: - 1. Column
+            VStack(alignment: .leading, spacing: 4) {
+                HStack {
+                    Image("icon-toasts")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Toasts")
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image("icon-tacos")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Tacos")
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image("icon-salads")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Salads")
+                }
+                
+                Divider()
+                
+                HStack {
+                    Image("icon-halfavo")
+                        .resizable()
+                        .modifier(IconModifier())
+                    Spacer()
+                    Text("Spreads")
+                }
+                
+            }
+            
+            //MARK: - 2. Column
+            
+            VStack(alignment: .center, spacing: 16) {
+                HStack {
+                    Divider()
+                }
+                Image(systemName: "heart.circle")
+                    .imageScale(.large)
+                    .font(Font.title.weight(.ultraLight))
+                HStack {
+                    Divider()
+                }
+            }
+            
+            //MARK: - 3. Column
+            
+            VStack(alignment: .trailing, spacing: 4) {
+                HStack {
+                    Text("Guacamole")
+                    Spacer()
+                    Image("icon-guacamole")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("Sandwiches")
+                    Spacer()
+                    Image("icon-sandwiches")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("Soup")
+                    Spacer()
+                    Image("icon-soup")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+                
+                Divider()
+                
+                HStack {
+                    Text("Smoothie")
+                    Spacer()
+                    Image("icon-smoothies")
+                        .resizable()
+                        .modifier(IconModifier())
+                }
+            }
+        }
+        .font(.system(.callout, design: .serif))
+        .foregroundColor(.gray)
+        .padding(.horizontal)
+        .frame(maxHeight: 220)
+    }
+}
+
+struct IconModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .frame(width: 42, height: 42, alignment: .center)
+    }
+}
+
+//MARK: - Preview
+struct DishesView_Previews: PreviewProvider {
+    static var previews: some View {
+        DishesView()
+            .previewLayout(.fixed(width: 414, height: 280))
+    }
+}
