4e8e137a8b0b5331b066aeefac0802ac826ae6bc
Yusuf Çağlar <faf7faf7@gmail.com>
2021-04-24 02:16:59 +0300

    Add Settings.

--- Avocado/View/SettingsView.swift
+++ Avocado/View/SettingsView.swift
@@ -8,8 +8,104 @@
 import SwiftUI
 
 struct SettingsView: View {
+    //MARK: - Properties
+    @State private var enableNotification: Bool = true
+    @State private var backgroundRefresh: Bool = false
+    
+    //MARK: - Body
+    
     var body: some View {
-        Text("Settings")
+        VStack(alignment: .center, spacing: 0) {
+            //MARK: - Header
+            VStack(alignment: .center, spacing: 5) {
+                
+                Image("avocado")
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.top)
+                    .frame(width: 100, height: 100, alignment: .center)
+                    .shadow(color: Color("ColorBlackTransparentLight"), radius: 8, x: 0, y: 4)
+                
+                Text("Avocados".uppercased())
+                    .font(.system(.title, design: .serif))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color("ColorGreenAdaptive"))
+            }
+            .padding()
+            
+            Form {
+                //MARK: - Section #1
+                
+                Section(header: Text("General Settings")) {
+                    Toggle(isOn: $enableNotification) {
+                        Text("Enable Notification")
+                    }
+                    
+                    Toggle(isOn: $backgroundRefresh) {
+                        Text("Background refresh")
+                    }
+                }
+                
+                //MARK: - Sextion #2
+                
+                Section(header: Text("Application")) {
+                    if enableNotification {
+                        HStack {
+                            Text("Product")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Avocado Recipes")
+                        }
+                        
+                        HStack {
+                            Text("Compability")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("iPhone & iPad")
+                        }
+                        
+                        HStack {
+                            Text("Developer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Yusuf Caglar")
+                        }
+                        
+                        HStack {
+                            Text("Designer")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Robert Petras")
+                        }
+                        
+                        HStack {
+                            Text("Github")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("github.com/yusufcaglar")
+                        }
+                        
+                        HStack {
+                            Text("Version")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("1.0")
+                        }
+                    } else {
+                        HStack {
+                            Text("Personal message")
+                                .foregroundColor(.gray)
+                            Spacer()
+                            Text("Happy Coding!")
+                        }
+                    }
+                    
+                    
+                    
+                }
+            }
+        }
+        .frame(maxWidth: 640)
     }
 }
 
